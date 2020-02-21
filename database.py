c34fe5ce816e2525fbe54401e946a422fbdeeb07
Neboer <rubinposter@gmail.com>
2020-02-21 10:12:48 +0800

    已经可以连接数据库并创建表。首次建表请删去main函数中建表的注释，删去了.idea文件夹

--- database.py
+++ database.py
@@ -2,26 +2,17 @@ from peewee import *
 
 
 # internal method
-def connect_to_db(mysql_obj):
+def getDataBase(mysql_obj):
     mysql_db = MySQLDatabase(mysql_obj["db_name"], user=mysql_obj["username"], password=mysql_obj["password"],
                              host=mysql_obj["addr"], port=mysql_obj["port"])
-    return mysql_db
-
-
-def init_table(mysql_obj):
-    conn_database = connect_to_db(mysql_obj)
 
     class BaseModel(Model):
         class Meta:
-            database = conn_database
+            database = mysql_db
 
-    return BaseModel
-
-
-def data_record_table(BaseModel):
     class DataRecord(BaseModel):
         device_id = IntegerField(primary_key=True)
         create_time = DateTimeField()
         data_content = CharField()
 
-    return
+    return mysql_db, DataRecord
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from sanic import Sanic
 from sanic.response import json
-from database import init_table, data_record_table
+from database import getDataBase
 from read_config import get_config
+from datetime import datetime
 
 app = Sanic()
 
@@ -13,6 +14,13 @@ async def test(request):
 
 if __name__ == "__main__":
     config = get_config()
-    table = init_table(config["mysql"])
+    dataBase, dataTable = getDataBase(config["mysql"])
+    dataBase.connect()
+    # dataBase.create_tables([dataTable])
+    dataTable.create(device_id='12345', create_time=datetime.now(), data_content="sdfasdasd")
 
+    db_settings = {
+        "database": dataTable
+    }
+    app.config.update(db_settings)
     app.run(host="0.0.0.0", port=8000)

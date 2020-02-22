50989c4dc674dd9d5817386bad048ddcee6bad75
wangjue <a1483423207@163.com>
2020-02-23 02:20:12 +0800

    1.完善了数据库的字段;2.开始写记录区块类

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ async def test(request):
 
 if __name__ == "__main__":
     config = get_config()
-    dataBase, dataTable = getDataBase(config["mysql"])
+    dataBase, userTable, recordTable = getDataBase(config["mysql"])  # 这里我觉得record似乎更合适
     dataBase.connect()
-    # dataBase.create_tables([dataTable])
-    dataTable.create(device_id='12345', create_time=datetime.now(), data_content="sdfasdasd")
+    # dataBase.create_tables([userTable,recordTable])
+    # userTable.create(device_id='123345', username='Jawa', password="what_*s_t*e_b**t_language_in_t*e_world")
+    # recordTable.create(device_id='1234', create_time=datetime.now(), data_content="sdfasdasd" ,hash = "?")
 
     db_settings = {
-        "database": dataTable
+        "database": recordTable  # 这里我感觉似乎没啥用，就先不动了
     }
     app.config.update(db_settings)
     app.run(host="0.0.0.0", port=8000)

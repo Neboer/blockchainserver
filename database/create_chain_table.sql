be2cc0c1118b1942ec84e7674bca11169d4819ac
Neboer <rubinposter@gmail.com>
2020-03-05 11:05:12 +0800

    现在，数据库可以接受区块的存储了。存储区块的时候，只需要保存新加入区块的数据就可以，区块的哈希值会自动计算并保存入库，然后再调用某个过程，区块的previous_hash也可以完成计算了。

---
+++ database/create_chain_table.sql
@@ -0,0 +1,21 @@
+/*create_time是unix timestamp类型的数据。在创建的时候，采集到的实际时间仅仅精确到(3)。最后一位是从0到9的数字，根据数据抵达到某个特定终端的顺序来排序。*/
+create table blocks
+(
+    id           int auto_increment primary key ,
+    device_id    varchar(20),
+    create_time  timestamp(4),
+    data_content varchar(255),
+    hash         binary(20) GENERATED ALWAYS as (unhex(sha1(concat(device_id, create_time, data_content)))) virtual ,
+    prev_hash    binary(20)
+);
+
+DELIMITER $$
+/*prev_hash并不是自动生成的。它的值应该是插入的。在insert之后，还应该执行这条语句来更新previous_hash值。*/
+create procedure wrapped_insert()
+begin
+    set @current_id = (select id from test.blocks where prev_hash is null order by id limit 1);
+    set @prev = (select prev_hash from test.blocks where id = @current_id-1);
+    update blocks set prev_hash = @prev := unhex(sha1(concat(hash, @prev))) where id >= @current_id;
+end;
+
+DELIMITER ;

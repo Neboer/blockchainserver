746e117791acb78e991cf8663ec54117d316e144
Neboer <rubinposter@gmail.com>
2020-03-14 13:09:18 +0800

    小区数据广播模块

--- blockchain/record.py
+++ blockchain/record.py
@@ -1,6 +1,7 @@
 # Record类是一个记录的抽象内容。这个类具有如下特征：
 # 自创建之后，类的实例对象保存在内存，在收到“共识”请求后把数据统一编码发往 服务器。
 
+
 class Record():
     def __init__(self, serial_number, device_id, create_time, data):
         # create_time是unix-timestamp的形式，应该是一个四位小数的浮点数。
--- database/add_block.py
+++ database/add_block.py
@@ -1,8 +1,8 @@
 # 配合record，做底层操作，完成对数据库记录的增加。
 def write_many(record_list, cursor, database):
     for record in record_list:
-        cursor.execute("INSERT INTO test.datarecord(device_id,create_time,data_content,hash,prev_hash) values "
-                       "(%s, %s, %s, (sha1(concat(device_id,create_time,data_content))), "
-                       "(sha1(concat(hash,(SELECT prev_hash from datarecord order by id desc limit 1)))))",
-                       (record.device_id, record.create_time, record.data,))
-        database.commit()
+        cursor.execute("INSERT INTO blocks (device_id, create_time, data_content) values (%s, %s, %s)",
+                       (record.device_id, record.create_time, record.data))
+    database.commit()
+    cursor.execute("CALL wrapped_insert()")
+    database.commit()
